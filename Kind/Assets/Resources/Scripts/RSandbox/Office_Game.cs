e776ec32ec7e501eb40b1813f451b9caa897b4ee
shumwengsang <ballwallcallall@gmail.com>
2018-09-08 14:38:01 -0700

    Added office visual, adding cloud

--- Kind/Assets/Resources/Scripts/RSandbox/Office_Game.cs
+++ Kind/Assets/Resources/Scripts/RSandbox/Office_Game.cs
@@ -19,6 +19,8 @@ public class Office_Game : MonoBehaviour {
 
     public void OnValueChange(string value)
     {
+        
+
         //Handle player sound
         if(enumerator != null)
         {
