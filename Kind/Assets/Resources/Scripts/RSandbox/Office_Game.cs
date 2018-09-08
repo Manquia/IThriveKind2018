aa9767cf82b9ee0e35ede0ff1f941aa128b07018
shumwengsang <ballwallcallall@gmail.com>
2018-09-08 11:53:02 -0700

    Fade in for RsandBox

--- Kind/Assets/Resources/Scripts/RSandbox/Office_Game.cs
+++ Kind/Assets/Resources/Scripts/RSandbox/Office_Game.cs
@@ -8,12 +8,17 @@ public class Office_Game : MonoBehaviour {
     public Button end_game_button;
 
     public GameObject screen_transition_object;
-
+    
     public void OnValueChange(string value)
     {
         if(value.Length > game_end_string_length)
         {
             end_game_button.interactable = true;
         }
+    }
+
+    public void OnButtonPressed()
+    {
+        FFMessage<TriggerFade>.SendToLocal(new TriggerFade());
     }
 }
