523372cd3236095991e5ac40d0dfd096af0ce5be
shumwengsang <ballwallcallall@gmail.com>
2018-09-08 11:42:44 -0700

    Static_Var update

--- Kind/Assets/Resources/Scripts/RSandbox/Office_Game.cs
+++ Kind/Assets/Resources/Scripts/RSandbox/Office_Game.cs
@@ -7,6 +7,8 @@ public class Office_Game : MonoBehaviour {
     public int game_end_string_length = 50;
     public Button end_game_button;
 
+    public GameObject screen_transition_object;
+
     public void OnValueChange(string value)
     {
         if(value.Length > game_end_string_length)
--- Kind/Assets/Resources/Scripts/RSandbox/Static_Var.cs
+++ Kind/Assets/Resources/Scripts/RSandbox/Static_Var.cs
@@ -3,14 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Static_Var : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    static int Money = 0;
 }
