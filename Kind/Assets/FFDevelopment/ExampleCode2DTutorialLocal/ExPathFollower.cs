92a53833306e8e2e1aa3ac6fffb1215dbeef8bfb
Manquia <Micahtim@gmail.com>
2018-09-09 15:41:01 -0700

    Fixed path so it has the new code... not the stuff I wrote like 3 years ago...

--- Kind/Assets/FFDevelopment/ExampleCode2DTutorialLocal/ExPathFollower.cs
+++ Kind/Assets/FFDevelopment/ExampleCode2DTutorialLocal/ExPathFollower.cs
@@ -9,38 +9,39 @@ public struct PathFollowerCompletedLoopEvent
 public class ExPathFollower : FFComponent {
 
     public float distance = 0.0f;
-    [Range(0.1f, 20.0f)]
     public float speed = 1.0f;
 
     private int loopCounter = 0;
 
     public FFPath PathToFollow;
 
-    void Awake()
-    {
-    }
-
-	// Use this for initialization
-	void Start () {
-	}
+    void Awake(){}
+    void Start(){}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate ()
+    {
         distance += Time.fixedDeltaTime * speed;
 
         var path = PathToFollow.GetComponent<FFPath>();
-        if(path)
+        
+        if (path)
         {
+            var PrevPos = path.PointAlongPath(distance - 0.01f);
             var position = path.PointAlongPath(distance);
+            var vecForward = Vector3.Normalize(position - PrevPos);
+
             transform.position = position;
-            if((int)(distance % path.PathLength) > loopCounter)
+            transform.up = vecForward;
+
+            if ((int)(distance / path.PathLength) > loopCounter)
             {
-                loopCounter = (int)(distance % path.PathLength);
+                loopCounter = (int)(distance / path.PathLength);
                 PathFollowerCompletedLoopEvent e;
                 e.distTraveled = path.PathLength;
-                FFMessage<PathFollowerCompletedLoopEvent>.SendToLocal(e);
+                FFMessageBoard<PathFollowerCompletedLoopEvent>.Send(e, gameObject);
             }
         }
-	}
+    }
 
 }
