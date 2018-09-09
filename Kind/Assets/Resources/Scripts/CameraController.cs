4c8300fcc03d1f5f0d6bd12f4d4de74f5df75911
Manquia <Micahtim@gmail.com>
2018-09-09 15:11:22 -0700

    SPELLING IS MAGIC!!!!

--- Kind/Assets/Resources/Scripts/CameraController.cs
+++ Kind/Assets/Resources/Scripts/CameraController.cs
@@ -14,7 +14,7 @@ public class CameraController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void FixedUpdat ()
+	void FixedUpdate ()
     {
         if (player == null)
             return;
