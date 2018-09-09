ccda5ff5fc3a8d8d483ae63b265a9f78eeff4994
Manquia <Micahtim@gmail.com>
2018-09-08 19:38:14 -0700

    Added a bunch of stuff

---
+++ Kind/Assets/Resources/Scripts/ActivateUsableObjects/AnimateOnActivate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateOnActivate : FFComponent {
+
+    public Animation animator;
+    public Animation[] animations;
+
+
+    FFAction.ActionSequence animSeq;
+	// Use this for initialization
+	void Start () {
+        animSeq = action.Sequence();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Kind/Assets/Resources/Scripts/ActivateUsableObjects/FollowOnActivate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowOnActivate : MonoBehaviour
+{
+    public float distance = 0.0f;
+    [Range(0.1f, 20.0f)]
+    public float speed = 1.0f;
+
+    private int loopCounter = 0;
+
+    public FFPath PathToFollow;
+    public bool moving = false;
+    
+    // Use this for initialization
+    void Start()
+    {
+        FFMessageBoard<ActivateUsableObject>.Connect(OnActivate, gameObject);
+    }
+    private void OnDestroy()
+    {
+        FFMessageBoard<ActivateUsableObject>.Disconnect(OnActivate, gameObject);
+    }
+
+    private int OnActivate(ActivateUsableObject e)
+    {
+        moving = true;
+        return 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (moving)
+        {
+            distance += Time.fixedDeltaTime * speed;
+
+            var path = PathToFollow.GetComponent<FFPath>();
+            if (path)
+            {
+                var position = path.PointAlongPath(distance);
+                transform.position = position;
+                if ((int)(distance % path.PathLength) > loopCounter)
+                {
+                    loopCounter = (int)(distance % path.PathLength);
+                    PathFollowerCompletedLoopEvent e;
+                    e.distTraveled = path.PathLength;
+                    FFMessage<PathFollowerCompletedLoopEvent>.SendToLocal(e);
+                }
+            }
+        }
+    }
+}
