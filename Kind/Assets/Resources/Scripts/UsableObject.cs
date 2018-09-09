3d34a32b57ad211ca050a139fb55c5a14409d20f
Manquia <Micahtim@gmail.com>
2018-09-08 17:50:11 -0700

    Changed the name!!!

--- Kind/Assets/Resources/Scripts/UsableObject.cs
+++ Kind/Assets/Resources/Scripts/UsableObject.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VendingMachine : MonoBehaviour {
+public class UsableObject : MonoBehaviour {
     
     public PlayerMessage QueryPM;
     public PlayerMessage []inProgressPMs;
